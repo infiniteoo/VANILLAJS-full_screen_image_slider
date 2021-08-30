af978590793cb6e861d6e3bc3abafbc90b102d44
Troy Dorman <troydorman@gmail.com>
2021-08-30 16:18:47 -0600

    defined some variables

---
+++ main.js
@@ -0,0 +1,4 @@
+let sliderImages = document.querySelectorAll(".slide");
+let arrowRight = document.querySelector("#arrow-right");
+let arrowLeft = document.querySelector("#arrow-left");
+let current = 0;
