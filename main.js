d2a3a0c560c05b577a48512b7c705e221d973576
Troy Dorman <troydorman@gmail.com>
2021-08-30 16:20:05 -0600

    created reset function to clear all images

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ let sliderImages = document.querySelectorAll(".slide");
 let arrowRight = document.querySelector("#arrow-right");
 let arrowLeft = document.querySelector("#arrow-left");
 let current = 0;
+
+// clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
